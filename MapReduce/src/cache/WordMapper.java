f6719169a9cfb23283ae6f3549655dcb57761eff
kkyu <gusrb8925@naver.com>
2021-06-01 09:28:52 +0900

    21-06-01 hadoop cache

---
+++ MapReduce/src/cache/WordMapper.java
@@ -0,0 +1,44 @@
+package cache;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	boolean caseSensitive = false;
+
+	@Override
+	public void map(LongWritable key, Text value, Context context)
+			throws IOException, InterruptedException {
+
+		String line = value.toString();
+
+		for (String word : line.split("\\W+")) {
+			if (word.length() > 0) {
+
+				String letter = "";
+
+				if (caseSensitive) {
+					letter = word.substring(0, 1).toUpperCase();
+				} else {
+					letter = word.substring(0, 1).toLowerCase();
+				}
+				context.write(new Text(letter), new IntWritable(1));
+			}
+		}
+
+	}
+
+	@Override
+	protected void setup(Context context) throws IOException,
+			InterruptedException {
+		Configuration conf = context.getConfiguration();
+		caseSensitive = conf.getBoolean("caseSensitive", false);
+	}
+
+}
